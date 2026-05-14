229756d189c5deb8661a191c9c50c6fc676e382e
shivam19042001 <18ec.shivamthacker@gmail.com>
2026-05-14 10:11:19 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.0",
+    author="Shivam",
+    author_email="thackershivam555@gmail.com",
+    packages=find_packages()
+)
